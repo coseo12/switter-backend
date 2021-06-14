b4346daffcabfd14671ef2438eb435e072b04d98
coseo12 <momo_12@naver.com>
2021-06-14 17:49:17 +0900

    modify MVC

---
+++ src/controller/tweet.js
@@ -0,0 +1,38 @@
+import * as tweetRepository from '../data/tweet.js';
+
+export async function getTweets(req, res, next) {
+  const username = req.query.username;
+  const data = await (username
+    ? tweetRepository.getByUsername(username)
+    : tweetRepository.getAll());
+  res.status(200).json(data);
+}
+
+export async function getTweetById(req, res, next) {
+  const id = req.params.id;
+  const tweet = await tweetRepository.getById(id);
+  if (tweet) {
+    res.status(200).json(tweet);
+  } else {
+    res.status(404).json({ message: `Tweet id(${id}) not found` });
+  }
+}
+
+export async function createTweet(req, res, next) {
+  const tweet = await tweetRepository.created(req.body);
+  res.status(201).json(tweet);
+}
+
+export async function updateTweet(req, res, next) {
+  const tweet = await tweetRepository.updated(req.params.id, req.body.text);
+  if (tweet) {
+    res.status(200).json(tweet);
+  } else {
+    res.status(404).json({ message: `Tweet id(${id}) not found` });
+  }
+}
+
+export async function deleteTweet(req, res, next) {
+  await tweetRepository.remove(req.params.id);
+  res.sendStatus(204);
+}
---
+++ src/data/tweet.js
@@ -0,0 +1,55 @@
+let tweets = [
+  {
+    id: '1',
+    text: 'test data1',
+    createdAt: Date.now().toString(),
+    name: 'Bob',
+    username: 'bob',
+    url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
+  },
+  {
+    id: '2',
+    text: 'test data2',
+    createdAt: Date.now().toString(),
+    name: 'Seo',
+    username: 'Seo',
+    url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
+  },
+];
+
+export async function getAll() {
+  return tweets;
+}
+
+export async function getByUsername(username) {
+  return tweets.filter(tweet => tweet.username === username);
+}
+
+export async function getById(id) {
+  return tweets.find(tweet => tweet.id === id);
+}
+
+export async function created({ text, name, username, url }) {
+  const tweet = {
+    id: Date.now().toString(),
+    text,
+    name,
+    username,
+    url: url ? url : '',
+    createdAt: Date.now().toString(),
+  };
+  tweets = [tweet, ...tweets];
+  return tweet;
+}
+
+export async function updated(id, text) {
+  const tweet = getById(id);
+  if (tweet) {
+    tweet.text = text;
+  }
+  return tweet;
+}
+
+export async function remove(id) {
+  tweets = tweets.filter(tweet => tweet.id !== id);
+}
