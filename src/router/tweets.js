64fe535401c44fce1eb46d831fe868874cd1ef0f
coseo12 <momo_12@naver.com>
2021-06-14 19:10:59 +0900

    add validator

--- src/router/tweets.js
+++ src/router/tweets.js
@@ -1,8 +1,22 @@
 import express from 'express';
 import 'express-async-error';
 import * as tweetController from '../controller/tweet.js';
+import { body } from 'express-validator';
+import { validate } from '../middleware/validator.js';
 
 const router = express.Router();
+
+// validation
+// sanitization
+// Contract Testing: Client-Server
+
+const validateTweet = [
+  body('text')
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage(`Text should be at least 3 char`),
+  validate,
+];
 
 // GET /tweets
 // GET /tweets?username=:username
@@ -12,10 +26,10 @@ router.get('/', tweetController.getTweets);
 router.get('/:id', tweetController.getTweetById);
 
 // POST /tweets
-router.post('/', tweetController.createTweet);
+router.post('/', validateTweet, tweetController.createTweet);
 
 // PUT /tweets/:id
-router.put('/:id', tweetController.updateTweet);
+router.put('/:id', validateTweet, tweetController.updateTweet);
 
 // DELETE /tweets/:id
 router.delete('/:id', tweetController.deleteTweet);
