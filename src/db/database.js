ffd163c87040ef46fd6997e6d55def54ff241ac2
coseo12 <momo_12@naver.com>
2021-06-18 18:35:41 +0900

    deploy

--- src/db/database.js
+++ src/db/database.js
@@ -4,7 +4,6 @@ import { config } from '../config.js';
 const { host, user, database, password, uri } = config.db;
 const connect = uri ? [uri] : [database, user, password];
 export const sequelize = new SQ.Sequelize(...connect, {
-  host,
   dialect: 'postgres',
   logging: false,
   dialectOptions: {
